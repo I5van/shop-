800c1be164a28b9441fb99e9fe82a2424b26311c
I5van <n5im@yandex.ru>
2023-03-16 16:17:03 +0300

    nbuvic

--- index.js
+++ index.js
@@ -1,57 +1,82 @@
 const { response } = require('express')
-const express=require('express')
+const express = require('express')
 const app = express()
 const port = 3000
-app.get("/",(request,response) =>{
-    response.sendFile(__dirname+"/index/index.html")
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/index/index.html")
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Start to listen port")
 })
 app.use(express.static("static"))
-app.get("/catalogue",(request,response) =>{
-    response.sendFile(__dirname+"/index/catalogue.html")
+app.get("/catalogue", (request, response) => {
+    response.sendFile(__dirname + "/index/catalogue.html")
 })
-let product1={
-    count:5000,
-    cost:1000,
-    category: "balls"
-}
-let product2 ={
-    count : 3000,
-    cost:2500,
-    category:"gloves"
-}
-let product3 ={
-    count: 1000,
-    cost: 10000,
-    category: "boots"
-}
-let products =[product1,product2,product3]
-let category = "balls"
-for(let i =0; i<products.length;i++){
-    if(products[i].category== category){
-        console.log(products[i])
-    }
-}
+// let product1={
+//     count:5000,
+//     cost:1000,
+//     category: "balls"
+// }
+// let product2 ={
+//     count : 3000,
+//     cost:2500,
+//     category:"gloves"
+// }
+// let product3 ={
+//     count: 1000,
+//     cost: 10000,
+//     category: "boots"
+// }
+// let products =[product1,product2,product3]
+// let category = "balls"
+// for(let i =0; i<products.length;i++){
+//     if(products[i].category== category){
+//         console.log(products[i])
+//     }
+// }
 
-let array = ["title:Гарри Поттер и философский камень", "releasedYear:2001", "director:Chris Columbus", "mainActor:Daniel Jacob Radcliffe"]
-function createObject(array){
-    let obj={}
-    let property=""
-    let value=""
-    for(let i =0;i<array.length;i++){
-        for(let j = 0;j<array[i].length;j++){
-            if(array[i][j]==":") {
-            property = array[i].slice(0,j)
-            value =  array[i].slice(j+1,array[i].length)
-        }
-    }
-        obj[property]=value
-        
-    }
-    return obj
-}
-let n =createObject(array)
-console.log(n)
+// let array = ["title:Гарри Поттер и философский камень", "releasedYear:2001", "director:Chris Columbus", "mainActor:Daniel Jacob Radcliffe"]
+// function createObject(array){
+//     let obj={}
+//     let property=""
+//     let value=""
+//     for(let i =0;i<array.length;i++){
+//         for(let j = 0;j<array[i].length;j++){
+//             if(array[i][j]==":") {
+//                 property = array[i].slice(0,j)
+//                 value =  array[i].slice(j+1,array[i].length)
+//             }
+//         }
+//         obj[property]=value
 
+//     }
+//     return obj
+// }
+// let n =createObject(array)
+// console.log(n)
+
+// function CreateObj() {
+//    let urlParams = urlParams.slice(1,urlParams.length)
+//     let massiveUrlParams = []
+//     massiveUrlParams = urlParams.split(`&`)
+
+//     let obj = {}
+//     let property = ""
+//     let value = []
+//     for (let i = 0; i < massiveUrlParams.length; i++) {
+//         for (let j = 0; j < massiveUrlParams[i].length; j++) {
+//             if (massiveUrlParams[i][j] == "=") {
+//                 console.log(massiveUrlParams)
+//                 property = massiveUrlParams[i].slice(0, j)
+//                obj[property]= massiveUrlParams[i].slice(j + 1, massiveUrlParams[i].length).split(",")
+//                 break
+//             }
+
+//         }
+       
+//     }
+//     return obj
+// }
+
+// let n = CreateObj()
+// console.log(n)
--- static/js/catalogue.js
+++ static/js/catalogue.js
@@ -1,12 +1,26 @@
-console.log(window.location.search)
-let loc = window.location.search
-let categoryName = ""
-for(i=0;i<loc.length;i++){
-    if(i>9){
-        if(loc[i]=="/"){
-            return
+function createParamObj() {
+    let urlParams=window.location.search
+    urlParams = urlParams.slice(1,urlParams.length)
+    let massiveUrlParams = []
+    massiveUrlParams = urlParams.split(`&`)
+
+    let obj = {}
+    let property = ""
+    let value = []
+    for (let i = 0; i < massiveUrlParams.length; i++) {
+        for (let j = 0; j < massiveUrlParams[i].length; j++) {
+            if (massiveUrlParams[i][j] == "=") {
+                console.log(massiveUrlParams)
+                property = massiveUrlParams[i].slice(0, j)
+               obj[property]= massiveUrlParams[i].slice(j + 1, massiveUrlParams[i].length).split(",")
+                break
+            }
+
         }
-        categoryName=categoryName+loc[i]      
+       
     }
+    return obj
 }
-console.log(categoryName)
+
+let params = createParamObj()
+console.log(params)
